eed6ce5e3a4d7035e641159490e3564ac62fa614
Rahul <rahulsud48@gmail.com>
2022-07-02 13:04:15 +0530

    rotated array efficient

--- search/bs_rotated_array.cpp
+++ search/bs_rotated_array.cpp
@@ -2,6 +2,7 @@
 
 int bs_rotated_arr(int* arr, int n, int x)
 {
+    // this has time complexity of log(n) + k
     int n_ = n-1;
 
     int low;
@@ -50,13 +51,55 @@ int bs_rotated_arr(int* arr, int n, int x)
     return -1;
 }
 
+int bs_arr_sorted_efficient(int* arr, int n, int x)
+{
+    int high = n-1, low = 0;
+    int mid;
+
+    while (low <= high)
+    {
+        mid = (low+high)/2;
+
+        if (arr[mid] == x)
+        {
+            return mid;
+        }
+
+        if (arr[mid] > arr[low]) // sorted condition
+        {
+            if (x >= arr[low] && x < arr[mid])
+            {
+                high = mid-1;
+            }
+            else
+            {
+                low = mid+1;
+            }
+        }
+        else
+        {
+            if (x > arr[mid] && x <= arr[high])
+            {
+                low = mid+1;
+            }
+            else
+            {
+                high = mid-1;
+            }
+        }
+    }
+
+
+	return -1;
+}
+
 int main()
 {
     int arr[] = {3,4,5,6,7,8,9,0,1,2};
     int n = 10;
-    int x = 3;
+    int x = 5;
     
-    int idx = bs_rotated_arr(arr, n, x);
+    int idx = bs_arr_sorted_efficient(arr, n, x);
 
     std::cout<<"The index is: " << idx << "\n";
 }
