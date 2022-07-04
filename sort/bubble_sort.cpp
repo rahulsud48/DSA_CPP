2c1a95fa7550a6f6acea1482e5d1502f5f8d5dec
Rahul <rahulsud48@gmail.com>
2022-07-04 09:01:14 +0530

    bubble sort

---
+++ sort/bubble_sort.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+
+void show_array(int* arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<"\n";
+}
+
+int bubble(int* arr, int n)
+{
+    for (int j = n-1; j >=1; j--)
+    {
+        for (int i = 0; i < j; i++)
+        {
+            if (arr[i] > arr[i+1])
+            {
+                int x;
+                x = arr[i+1];
+                arr[i+1] = arr[i];
+                arr[i] = x;
+            }
+            
+        }
+        show_array(arr, n);
+    }
+
+}
+
+int main()
+{
+    int arr[] = {9,8,7,6,5,4,3,2,1,0};
+    int n = 10;
+    show_array(arr, n);
+    bubble(arr, n);
+}
