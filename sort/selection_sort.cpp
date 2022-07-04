241c4a331095e25bd42d571e20ee64d9e2f815d0
Rahul <rahulsud48@gmail.com>
2022-07-04 12:02:51 +0530

    selection sort working

--- sort/selection_sort.cpp
+++ sort/selection_sort.cpp
@@ -11,13 +11,38 @@ void show_array(int* arr, int n)
 
 void selection(int* arr, int n)
 {
+
+    for (int j = n-1; j > 0; j--)
+    {
     
+        int max_num = -99999;
+        int max_pos;
+        
+        for (int i = 0; i <= j; i++)
+        {
+            if (arr[i] >= max_num)
+            {
+                max_num = arr[i];
+                max_pos = i;
+            }
+        }
+        std::cout<< "\n";
+
+        // swap
+        arr[max_pos] = arr[j];
+        arr[j] = max_num;
+        std::cout<<"Max number: " << max_num << "\n";
+        std::cout<<"Max position: " << max_pos << "\n";
+        std::cout<<"Last position: " << j << "\n";
+        show_array(arr, n);
+    }
+
 }
 
 int main()
 {
-    int arr[] = {9,8,7,6,5,4,3,2,1,0};
+    int arr[] = {4,7,-2,9,-5,3,1,0,6,8};
     int n = 10;
-    show_array(arr, n);
+    // show_array(arr, n);
     selection(arr, n);
 }
