b81070de347472c7965735947c19dd46468e196e
Rahul <rahulsud48@gmail.com>
2022-07-08 17:08:09 +0530

    two array problem

---
+++ sort/sort_sorted_added arrays.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+
+void show_array(int* arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        std::cout<<arr[i]<<" ";
+    }
+    std::cout<<"\n";
+}
+
+void sort(int* arr, int n, int low, int mid, int high)
+{
+    int j = mid + 1;
+    for (int i = 0; i < mid+1; i++)
+    {
+        while(arr[i] > arr[j])
+        {
+            std::cout<<arr[j] << " ";
+            j++;
+        }
+        std::cout<<arr[i]<<" ";
+        
+    }
+}
+
+int main()
+{
+    int arr[] = {10,15,20,11,30, 40};
+    int n = 6;
+    sort(arr, n, 0, 2, 4);
+    std::cout<<"\n";
+
+}
