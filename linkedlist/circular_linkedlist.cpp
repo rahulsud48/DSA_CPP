a0bb64f47c74b56b6837393dec6193ce87634cb8
Rahul <rahulsud48@gmail.com>
2022-08-23 15:10:03 +0530

    changes made

--- linkedlist/circular_linkedlist.cpp
+++ linkedlist/circular_linkedlist.cpp
@@ -11,8 +11,25 @@ struct Node
     }
 };
 
+Node* insert_begin(Node* curr, int x)
+{
+    Node* head = new Node(x);
+    Node* curr_ = curr;
+    while (curr_->next != curr)
+    {
+        curr_ = curr_->next;
+    }
+    curr_->next = head;
+    head->next = curr;
+    return head;
+}
+
 void traverse(Node* head)
 {
+    if (head == NULL)
+    {
+        return;
+    }
     Node* curr = head;
     std::cout<<curr->data<<" ";
     while (curr->next != head)
@@ -25,10 +42,11 @@ void traverse(Node* head)
 int main()
 {
     Node* head = new Node(10);
-    // head->next = new Node(5);
-    // head->next->next = new Node(20);
-    // head->next->next->next = new Node(15);
-    // head->next->next->next->next = head;
-    head->next = head;
+    head->next = new Node(5);
+    head->next->next = new Node(20);
+    head->next->next->next = new Node(15);
+    head->next->next->next->next = head;
+
+    head = insert_begin(head, 25);
     traverse(head);
 }
